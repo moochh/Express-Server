6c10043e25b235c58123f5aeea039d7cce1d3367
Jay Rome Rana <itaalyyy@gmail.com>
2024-09-29 13:41:09 +0800

    added user endpoint

--- index.cjs
+++ index.cjs
@@ -1,16 +1,46 @@
 // Import http library
 const http = require('http');
+const express = require('express');
+const app = express();
+
 // use env variable to define tcp/ip port with a default
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
 //create our server object
 const server = http.createServer();
+
+// Endpoint
+app.get('/users', (req, res) => {
+	const users = [
+		{
+			id: 1,
+			name: 'John Doe',
+			email: 'johndoe@example.com'
+		},
+		{
+			id: 2,
+			name: 'Jane Doe',
+			email: 'janedoe@example.com'
+		},
+		{
+			id: 3,
+			name: 'Bob Smith',
+			email: 'bobsmith@example.com'
+		}
+	];
+	res.json(users);
+});
+
 // We define a function that runs in response a request event
 server.on('request', (request, response) => {
 	// handle request based on method then URL
 	response.statusCode = 200;
-	response.write(process.env.api_key);
+	response.write('Hello World!');
 	response.end();
 });
+
 // get the server to start listening
 server.listen(PORT, (err) => {
 	// error checking
